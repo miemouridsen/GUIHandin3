6861dd2c1bbc85148500687b08d6be1615b4b0f4
miemouridsen <miemouridsen@hotmail.com>
2021-12-06 15:46:01 +0100

    Update AddExpense.vue

--- src/components/Model/AddExpense.vue
+++ src/components/Model/AddExpense.vue
@@ -1,27 +1,26 @@
 <template>
-    <select>
-        <option v-for="job in jobs" v-bind:key="job.efjobId">
-            <p>{{job.customer}}</p>
+    <select v-model="newExpense.jobId">
+        <option v-for="job in jobs" v-bind:key="job.efJobId">
+            <p>{{ job.customer }}</p>
         </option>   
     </select>
 
-    <br>
-    <form> 
-        <label>Expense description</label>
-        <input type="text" v-model="newExpense.text" required>
-        <label>price for job</label>
-        <input type="number" v-model="newExpense.amount" required>
-        <label>date</label>
-        <input type="date" v-model="newExpense.date" required>
+    <form @submit.prevent="addExpense"> 
+        <label>Expense Description</label>
+        <input type="text" v-model="newExpense.text" required/>
+        <label>Price of Expense</label>
+        <input type="number" v-model="newExpense.amount" required/>
+        <label>Date</label>
+        <input type="date" v-model="newExpense.date" required/>
+        <input type="submit" value="Add New Expense"/>
     </form>
-
-    <button v-on:click = "addExpense"> Add New Expense </button>
 </template>
 
 <script>
 import { addNewExpense } from "../../Services/ExpenseService.js";
-import {getJobs} from "../../Services/JobService.js"
-import {getUser} from "../../Services/LoginService.js"
+import { getJobs } from "../../Services/JobService.js"
+import { getUser } from "../../Services/LoginService.js"
+
 export default {
     async created() {
         this.jobs = await getJobs();
@@ -32,7 +31,7 @@ export default {
             newExpense: {
                 modelId: 0,
                 jobId: 0,
-                date: "2021-12-06T11:45:18.890Z",
+                date: "",
                 text: "",
                 amount: 0,
             },
@@ -42,10 +41,11 @@ export default {
     },
     methods: {
         async addExpense() {
+            //var date = new Date(this.newExpense.date);
+            //this.newExpense.date = "2021-12-12";
             await addNewExpense(this.newExpense);
         }
     }
-
 }
 </script>
 
